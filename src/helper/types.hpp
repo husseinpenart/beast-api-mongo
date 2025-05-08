99b95aeda224fe3e323307a9594066bc3101760b
husseinpenart <artistpenman1515@gmail.com>
2025-05-08 11:53:53 -0700

    helper types added too

---
+++ src/helper/types.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <boost/beast/http.hpp>
+#include <mongocxx/database.hpp>
+
+namespace http = boost::beast::http;
+
+// Define a more concise alias for commonly used request and response references
+using HttpReq = http::request<http::string_body>&;
+using HttpRes = http::response<http::string_body>&;
+using MongoDB = mongocxx::database&;
